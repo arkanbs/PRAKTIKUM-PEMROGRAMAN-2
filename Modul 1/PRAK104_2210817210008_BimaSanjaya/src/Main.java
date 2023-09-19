a55b5640a12ef09c3ccd41094c55dffd17b6e5b3
arkanbs <115327354+arkanbs@users.noreply.github.com>
2023-09-19 19:17:56 +0800

    Delete Modul 1 directory

--- Modul 1/PRAK104_2210817210008_BimaSanjaya/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String nama = "Bima Sanjaya",umur = "20";
-
-        System.out.println("Perkenlakan, Nama Saya "+nama+", Umur Saya "+umur);
-    }
-}
