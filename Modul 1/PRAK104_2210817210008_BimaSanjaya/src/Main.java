97d23cfff0021a33a0f8ba2f3ad3c3b51a84ec2f
arkanbs <115327354+arkanbs@users.noreply.github.com>
2023-09-19 18:09:41 +0800

    Add files via upload

---
+++ Modul 1/PRAK104_2210817210008_BimaSanjaya/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String nama = "Bima Sanjaya",umur = "20";
+
+        System.out.println("Perkenlakan, Nama Saya "+nama+", Umur Saya "+umur);
+    }
+}
