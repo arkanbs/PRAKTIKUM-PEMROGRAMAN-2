d440c98c73890aa25395f3bb9ed7c5083ba24b80
arkanbs <2210817210008@mhs.ulm.ac.id>
2023-10-17 19:59:51 +0800

    Commit

---
+++ Modul 2/PRAK203_2210817210008_BimaSanjaya/src/Pegawai.java
@@ -0,0 +1,23 @@
+public class Pegawai {
+    public String nama;
+
+    //Pada baris ini terjadi error karena tipe data yang tidak sesuai
+    //public char asal;
+    public String asal; //tipe data char kita ubah menjadi String
+    public String jabatan;
+    public int umur;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAsal() {
+        return asal;
+    }
+
+    //pada baris ini terjadi error karena tidak ada tipe data variabel dalam parameter
+    //public void setJabatan() {
+    public void setJabatan(String j) { //kita isikan tipe data String disertai variabelnya parameter
+        this.jabatan = j;
+    }
+}
---
+++ Modul 2/PRAK203_2210817210008_BimaSanjaya/src/Soal3Main.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+
+public class Soal3Main {
+    public static void main(String[] args) {
+        Pegawai p1 = new Pegawai();
+
+        //pada baris ini terjadi error karena kurangnya titik koma (;)
+        //p1.nama = "Roi"
+        p1.nama = "Roi"; //kita tambahkan titik koma (;) di belakangnya
+        p1.asal = "Kingdom of Orvel";
+        p1.setJabatan("Assasin");
+        //pada baris ini seharusnya kita buat properti untuk variabel umur
+        p1.umur = 17;
+
+        //print output kita ubah menjadi Nama
+        //System.out.println("Nama Pegawai: " + p1.getNama());
+        System.out.println("Nama: " + p1.getNama());
+        System.out.println("Asal: " + p1.getAsal());
+        System.out.println("Jabatan: " + p1.jabatan);
+        //print output untuk umur sebelumnya mengeluarkan nilai 0 karena belum memiliki properti
+        //selanjutnya kita tambahkan kata tahun setelah memanggil variabel umur
+        //System.out.println("Umur: " + p1.umur);
+        System.out.println("Umur: " + p1.umur + " tahun");
+    }
+}
